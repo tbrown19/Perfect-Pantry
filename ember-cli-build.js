6eb996f84ad4b85cf8d08183eda908cb5dcb147f
Tyler Brown <tbrown25@wisc.edu>
2016-11-05 17:13:47 -0500

    Landing page design and some other changes

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
+    fingerprint: {
+         prepend: 'https://perfect-pantry.firebaseapp.com'
+       }
   });
 
   // Use `app.import` to add additional libraries to the generated
